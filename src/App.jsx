02e4aae239600d7aff87effa849552356d9e8ff4
Michelle Appiah <michappiah67@gmail.com>
2023-10-20 17:00:12 +0000

    calculating cart items

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,7 @@ function App() {
   const [backgroundWhite, setBackgroundWhite] = useState(false);
   const [isBackgroundImage, setBackgroundImage] = useState(false);
   const [cartVisibility, setCartVisible] = useState(false);
-  const [itemsInCart, setItems] = useState([]);
-
-  
+  const [cartItems, setCartItems] = useState([]); 
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
@@ -25,29 +23,51 @@ function App() {
   }, []);
 
   const addToCart = (product) => {
-    setItems([...itemsInCart, product]);
+    const newItem = { ...product, count: 1 }; 
+    setCartItems([...cartItems, newItem]);
+  };
+
+  const removeProduct = (productId) => {
+    const updatedCart = cartItems.filter((item) => item.id !== productId);
+    setCartItems(updatedCart);
+  };
+
+  const quantityChange = (productId, newQuantity) => {
+    const updatedCart = cartItems.map((item) => {
+      if (item.id === productId) {
+        item.count = parseInt(newQuantity, 10);
+      }
+      return item;
+    });
+    setCartItems(updatedCart);
   };
 
   return (
     <div>
-      <Cart visibility={cartVisibility} products={itemsInCart} onClose={() => setCartVisible(false)}/>
+      <Cart
+        visibility={cartVisibility}
+        products={cartItems}
+        onClose={() => setCartVisible(false)}
+        removeProduct={removeProduct}
+        quantityChange={quantityChange} 
+      />
       <div className="navBar">
         <div className="boutiqueName">
           <h1>Chic Boutique</h1>
         </div>
         <input placeholder="search" />
         <div className="navBarButtons">
-          <HomeButton
-            setBackgroundImage={setBackgroundImage}
-           />
+          <HomeButton setBackgroundImage={setBackgroundImage} />
           <StoreButton
             setShowItemCards={setShowItemCards}
             setBackgroundWhite={setBackgroundWhite}
           />
           <div>
-            <button onClick={() => setCartVisible(true)} >
+            <button onClick={() => setCartVisible(true)}>
               cart
-              {itemsInCart.length > 0 && <span className='itemCount'>{itemsInCart.length}</span>}
+              {cartItems.length > 0 && (
+                <span className='itemCount'>{cartItems.length}</span>
+              )}
             </button>
           </div>
         </div>
@@ -55,7 +75,7 @@ function App() {
 
       {showItemCards && (
         <div className="itemCards">
-          <ItemCards products={products} addToCart={addToCart}/>
+          <ItemCards products={products} addToCart={addToCart} />
         </div>
       )}
     </div>
@@ -63,6 +83,7 @@ function App() {
 }
 
 export default App;
+
 
 
 
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,50 +1,83 @@
-import React from 'react'
+import React from 'react';
 
 function Cart({
-    visibility,
-    products,
-    removeProduct,
-    onClose,
-    quantityChange
+  visibility,
+  products,
+  removeProduct,
+  onClose,
+  quantityChange
 }) {
+  let totalCost = 0;
+
+  if (products.length > 0) {
+    totalCost = products.reduce((total, product) => {
+      return total + product.price * product.count;
+    }, 0);
+  }
+
   return (
-    <div className='cart' style={{display: visibility ? "block" : "none"}}>
+    <div className='cart' style={{ display: visibility ? 'block' : 'none' }}>
       <div className='shoppingCart'>
         <div className='cartHeader'>
-            <h2>Your Cart</h2>
-            <button className='btn close-btn' onClick={onClose}>X</button>
+          <h2>Your Cart</h2>
+          <button className='btn close-btn' onClick={onClose}>
+            X
+          </button>
         </div>
         <div className='cartItems'>
-            {products.length === 0 && (
-            <span className='emptyText'>
-                 Your Cart is Empty
-            </span>
-            )}
-            {products.map(product => (
-              <div className='cartItem' key={product.id}>
-                <img src={product.image} alt={product.title}/>
+          {products.length === 0 ? (
+            <span className='emptyText'>Your Cart is Empty</span>
+          ) : (
+            products.map((product, index) => (
+              <div className='cartItem' key={index}>
+                <img src={product.image} alt={product.title} />
                 <div className='itemInfo'>
-                    <h3>{product.title}</h3>
-                    <span className='itemPrice'>{product.price }$</span>
+                  <h3>{product.title}</h3>
+                  <span className='itemTotal'>
+                    {`Total: $${(product.price * product.count).toFixed(2)}`}
+                  </span>
                 </div>
-                 <select className='count' value={product.count} onChange={(event) => {
-                    quantityChange(product.id, event.target.value);
-                  }}>
-                    {
-                        [...Array(10).keys()]
-                        .map(number => {const num = number + 1;
-                        return <option value={num} key={num}>{num}</option>})
-                    }
-                 </select>
-                 <button className='btn removeBtn' onClick={() => removeProduct(product)}>remove</button>
-              </div> 
-            ))}
-            {products.length > 0 && <button className='btn checkoutBtn'>proceed to checkout</button>}
+                <select
+                  className='count'
+                  value={product.count}
+                  onChange={(event) => {
+                    const newCount = parseInt(event.target.value, 10);
+                    quantityChange(product.id, newCount);
+                  }}
+                >
+                  {[...Array(10).keys()].map((number) => {
+                    const num = number + 1;
+                    return (
+                      <option value={num} key={num}>
+                        {num}
+                      </option>
+                    );
+                  })}
+                </select>
+                <button
+                  className='btn removeBtn'
+                  onClick={() => removeProduct(product.id)}
+                >
+                  X
+                </button>
+              </div>
+            ))
+          )}
+          {products.length > 0 && (
+            <div className='cartTotal'>
+              <span className='totalLabel'>Total Of Items In Your Cart: </span>
+              <span className='totalAmount'>{`$${totalCost.toFixed(2)}`}</span>
+            </div>
+          )}
+          {products.length > 0 && (
+            <button className='btn checkoutBtn'>proceed to checkout</button>
+          )}
         </div>
       </div>
-        
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
+
+
